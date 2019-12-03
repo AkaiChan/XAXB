088b9d5ade5b96bed0713b12ece384e061c8271c
Bruce Chan <bruce20219@gmail.com>
2019-12-03 19:28:52 +0800

    add range function

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import random
 
-intAnswer = random.randint(1,100)
+intStart = input("Please input start of period:")
+intEnd = input("Please input end of period:")
+
+intAnswer = random.randint(int(intStart),int(intEnd))
+count = 0
 while True:
+	count += 1
 	intInput = input("Please inuput number: ")
 	intInput = int(intInput)
 	if intInput == intAnswer:
 		print("Correct")
+		print("Total ", count , " times")
 		break
 	else:
 		print("Incorrect!")
-		print(intAnswer)
 		if intInput > intAnswer:
 			print("Guess smaller")
 		else:
 			print("Guess bigger")
+	print("it's the " , count , " times")
